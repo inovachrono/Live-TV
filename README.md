18d7139ccdbf54bd42de41d72c6e4742f133f311
monuyadav016 <monuyadav016@gmail.com>
2020-04-03 00:11:43 +0530

    Updated README, added version.txt for on the fly channel update(BETA)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Live-TV
-A live TV streaming application
+A live TV streaming python application
+Requires VLC media player installed on your operating system to stream
+
+### Usage
+```
+$ python stream.py -file FILE_NAME_WITH_EXTENSION(.m3u)
+```
+For example a file already available with this repository with TV Channels specific to India can be run as 
+```
+$ python stream.py India.m3u
+```
+
+## License
+This program is licensed under [MIT License](https://github.com/monuyadav016/Live-TV/blob/master/LICENSE)
+
--- stream.py
+++ stream.py
@@ -1,4 +1,4 @@
-# import urllib.request
+import urllib.request
 import subprocess
 import argparse
 
@@ -21,7 +21,8 @@ class PlaylistParser():
                         return False
         except FileNotFoundError:
             return False
-
+    
+    # Parse the m3u file
     def parse_m3u(self, filename=None):
         if filename:
             self.filename = filename
@@ -39,42 +40,57 @@ class PlaylistParser():
                         channel_name = meta_data[-1]
                         channel["name"] = channel_name
                     elif (len(line) != 0):
-                        # pull channel path from all other, non-blank lines
+                        # pull channel link from all other, non-blank lines
                         link=line
                         channel["link"] = link
                         self.channels.append(channel)
+                        # make the channel dict empty for the new channel
                         channel = {}
         else:
             print("Not an m4u file")
     
+    # Display list to the user
     def list_channels(self):
         print("List of channels available are: ")
         for i, channel in enumerate(self.channels, 1):
             print(i, channel.get("name"))
     
+    # Returns the list of channels with {name, link}
     def get_channels(self):
         return self.channels
 
+# Managing the command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-file", help="Name of the .m3u file with extension")
 args = parser.parse_args()
 
+# Getting the current version info
+def update():
+    curr_ver = None
+    with open("version.txt", "r") as version_file:
+        curr_ver = float(version_file.readline().split("=")[-1])
+        print("Version : ", curr_ver)
+    response = urllib.request.urlopen("https://raw.githubusercontent.com/monuyadav016/Live-TV/master/version.txt").read
+    new_ver = float(response.split("=")[-1])
+    print("Latest version availble: ", new_ver)
+    print("Updating...")
+    if new_ver > curr_ver:
+        with open("version.txt", "w") as version_file:
+            version_file.wirte(new_ver)
+
 if args.file is None:
-    print("No filename provided")
+    print("No filename provided using default file India.m3u")
+    args.file = "India.m3u"
+playlist = PlaylistParser()
+playlist.parse_m3u(args.file)
+playlist.list_channels()
+channels = playlist.get_channels()
+channel_no = int(input("Enter the channel number: "))
+if channel_no > 0:
+    try:
+        channel = channels[channel_no - 1]
+        subprocess.run(["vlc", channel["link"]])
+    except Exception as e:
+        print(e)
 else:
-    print(args.file)
-    playlist = PlaylistParser()
-    playlist.parse_m3u(args.file)
-    playlist.list_channels()
-    channels = playlist.get_channels()
-    channel_no = int(input("Enter the channel number: "))
-    if channel_no > 0:
-        try:
-            channel = channels[channel_no - 1]
-            subprocess.run(["vlc", channel["link"]])
-        except Exception as e:
-            print(e)
-    else:
-        print("Invalid channel No")
-        
-    # print(playlist.get_channels())
+    print("Invalid channel No")
